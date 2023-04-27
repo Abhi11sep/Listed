c1b061b7300c452ac5afd6b7469aa072704bbc94
Abhishek Singh <abhi.rocks.11.9.1997@gmail.com>
2023-04-27 12:25:46 +0530

    ading chart data using API

--- board/src/Components/LineChart.jsx
+++ board/src/Components/LineChart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Line } from 'react-chartjs-2'
 import {
     Chart as ChartJS,
@@ -45,31 +45,78 @@ export const options = {
     },
 };
 
-const labels = ['Week 1', 'Week 2', 'Week 3', 'Week 4', ' '];
-
-export const data = {
-    labels,
-    datasets: [
-        {
-            label: 'Guest',
-            data: [200, 400, 200, 300, 220, 460],
-            borderColor: '#E9A0A0',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            yAxisID: 'y',
-            lineTension: 0.5,
-        },
-        {
-            label: 'User',
-            data: [100, 400, 150, 450, 180, 250],
-            borderColor: '#9BDD7C',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-            yAxisID: 'y',
-            lineTension: 0.5,
-        },
-    ],
-};
 
 const LineChart = () => {
+    const [data, setData] = useState({
+        labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', ' '],
+        datasets: [
+            {
+                label: 'Guest',
+                data: [200, 400, 200, 300, 220, 460],
+                borderColor: '#E9A0A0',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                yAxisID: 'y',
+                lineTension: 0.5,
+            },
+            {
+                label: 'User',
+                data: [100, 400, 150, 450, 180, 250],
+                borderColor: '#9BDD7C',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                yAxisID: 'y',
+                lineTension: 0.5,
+            },
+        ],
+    })
+
+    useEffect(() => {
+        const fetchData = () => {
+            const url = 'https://mock-7-backend.onrender.com/users'
+            const dataSet1 = [];
+            const dataSet2 = [];
+            fetch(url, {
+                method: 'GET'
+            }).then(data => {
+                console.log('data', data)
+                const res = data.json()
+                return res
+            }).then(res => {
+                console.log('data', res)
+                for (const val of res) {
+                    dataSet1.push(val.guest)
+                    dataSet2.push(val.user)
+                }
+                console.log(dataSet1, dataSet2)
+                setData({
+                    labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', ' '],
+                    datasets: [
+                        {
+                            label: 'Guest',
+                            data: dataSet1,
+                            borderColor: '#E9A0A0',
+                            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                            yAxisID: 'y',
+                            lineTension: 0.5,
+                        },
+                        {
+                            label: 'User',
+                            data: dataSet2,
+                            borderColor: '#9BDD7C',
+                            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                            yAxisID: 'y',
+                            lineTension: 0.5,
+                        },
+                    ],
+                })
+            }).catch(err => {
+                console.log('err', err)
+            })
+
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <div>
             <Line options={options} data={data} />
